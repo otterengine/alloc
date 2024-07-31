6ce89ff5300d4fb34e25e7558c1c03895bb49eac
Olle Lukowski <ollelukowski@hotmail.com>
2024-07-31 17:54:32 +0200

    Fixed windows issues?

--- src/align.c
+++ src/align.c
@@ -4,20 +4,43 @@ void *otter_align_forward(void *ptr, unsigned long align) {
   return otter_align_backward((void *)(((char *)ptr) + (align - 1)), align);
 }
 
-void *otter_align_backward(void *ptr, unsigned long align) {
-  char *p = (char *)ptr;
-  unsigned long offset = (unsigned long)p % align;
-  if (offset != 0) {
-    p -= offset;
+void *otter_align_backward(void *base, unsigned long align) {
+  unsigned long i;
+  unsigned char align_bytes[sizeof(unsigned long)];
+  unsigned char aligned_ptr_bytes[sizeof(void *)];
+  unsigned long *align_first = (void *)(&align_bytes[0]);
+  void **aligned_ptr_first = (void *)(&aligned_ptr_bytes[0]);
+
+  *align_first = align - 1;
+  *aligned_ptr_first = base;
+
+  for (i = (sizeof(void *) - sizeof(unsigned long));
+       i < sizeof(void *); i++) {
+    aligned_ptr_bytes[i] = aligned_ptr_bytes[i] & ~align_bytes[i % sizeof(unsigned long)];
   }
 
-  return (void *)p;
+  return *aligned_ptr_first;
 }
 
 int otter_is_aligned(void *ptr, unsigned long align) {
-  char *p = (char *)ptr;
+  unsigned char ptr_buf[sizeof(void *)];
+  unsigned char align_buf[sizeof(unsigned long)];
+  void **ptr_first = (void *)(&ptr_buf[0]);
+  unsigned long *align_first = (void *)(&align_buf[0]);
+  unsigned long i;
 
-  return (unsigned long)p % align == 0;
+  *ptr_first = ptr;
+  *align_first = align - 1;
+
+  /* we simply check if the least significant bytes overlap with the align */
+  for (i = (sizeof(void *) - sizeof(unsigned long));
+       i < sizeof(void *); i++) {
+    if ((ptr_buf[i] & ~align_buf[i % sizeof(unsigned long)]) != ptr_buf[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
 }
 
 unsigned long otter_align_size_forward(unsigned long size,
